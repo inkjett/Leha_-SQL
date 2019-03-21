1cf468400909a880a7812d8ffb92e3b05e72298f
inkjett <mail22sms@gmail.com>
2019-03-21 11:41:58 +0500

    многое сделал

--- SQL/Form1.Designer.cs
+++ SQL/Form1.Designer.cs
@@ -35,7 +35,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -43,12 +42,19 @@
             this.label5 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 45);
+            this.textBox1.Location = new System.Drawing.Point(13, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
@@ -56,7 +62,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(87, 115);
+            this.textBox2.Location = new System.Drawing.Point(13, 115);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
@@ -64,7 +70,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(86, 164);
+            this.textBox3.Location = new System.Drawing.Point(12, 164);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 2;
@@ -73,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 12);
+            this.label1.Location = new System.Drawing.Point(13, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 3;
@@ -82,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 90);
+            this.label2.Location = new System.Drawing.Point(10, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 4;
@@ -91,34 +97,25 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(87, 138);
+            this.label3.Location = new System.Drawing.Point(13, 138);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Пароль";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(13, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Insert";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(112, 206);
+            this.button2.Location = new System.Drawing.Point(12, 230);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "Select";
+            this.button2.Text = "Select User";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(13, 251);
+            this.button3.Location = new System.Drawing.Point(12, 201);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 8;
@@ -128,7 +125,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(112, 251);
+            this.button4.Location = new System.Drawing.Point(12, 440);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 9;
@@ -138,7 +135,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(667, 9);
+            this.label4.Location = new System.Drawing.Point(1266, 3);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(134, 13);
             this.label4.TabIndex = 10;
@@ -148,36 +145,80 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(667, 22);
+            this.label5.Location = new System.Drawing.Point(1266, 16);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(19, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "***";
             // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(360, 54);
+            this.dataGridView1.Location = new System.Drawing.Point(132, 45);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(408, 363);
+            this.dataGridView1.Size = new System.Drawing.Size(399, 363);
             this.dataGridView1.TabIndex = 13;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(555, 45);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(408, 363);
+            this.dataGridView2.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Select EVNT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(141, 9);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(122, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(978, 45);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(408, 363);
+            this.dataGridView3.TabIndex = 17;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(2, 298);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "окончательный массв";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 484);
+            this.ClientSize = new System.Drawing.Size(1408, 484);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -188,6 +229,8 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,7 +244,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
@@ -209,6 +251,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- SQL/Form1.cs
+++ SQL/Form1.cs
@@ -10,13 +10,22 @@ using System.Windows.Forms;
 using FirebirdSql.Data.FirebirdClient;
 using System.Collections;
 using System.IO;
+using Excel = Microsoft.Office.Interop.Excel
 
 namespace SQL
 {
     public partial class Form1 : Form
     {
+
+        //описание переменных
         FbConnection fb;
         List<List<string>> arr_user;
+        List<List<string>> arr_events;
+        List<List<string>> arr_of_work;
+        string date_to_request="0";
+        DateTime now = DateTime.Now;
+        
+        //------
         public Form1()
         {
             InitializeComponent();
@@ -26,34 +35,36 @@ namespace SQL
         {
 
         }
-               
 
-        private void button3_Click(object sender, EventArgs e)
+
+        //методы
+        public void method_connect_to_fb(TextBox Text_Path,TextBox Text_user,TextBox Text_pass, ref Label Label_out)// метод подключения к fd
         {
             FbConnectionStringBuilder fb_connect = new FbConnectionStringBuilder();
             fb_connect.Charset = "WIN1251"; // кодировка
-            fb_connect.UserID = textBox2.Text; // Логин
-            fb_connect.Password = textBox3.Text; // Пароль
-            fb_connect.Database = textBox1.Text; // путь до БД
+            fb_connect.UserID = Text_user.Text; // Логин
+            fb_connect.Password = Text_pass.Text; // Пароль
+            fb_connect.Database = Text_Path.Text; // путь до БД
             fb_connect.ServerType = 0; //  хз что такое  ----   указываем тип сервера (0 - "полноценный Firebird" (classic или super server), 1 - встроенный (embedded))
             fb = new FbConnection(fb_connect.ToString()); // открываем подключение, вставляя строку подключения 
             fb.Open();
             //FbDatabaseInfo fb_info = new FbDatabaseInfo(fb);
             //MessageBox.Show("Info: "+ fb_info.ServerClass+"\nVer: "+fb_info.ServerVersion);
-            if(fb.State == ConnectionState.Open)
+            if (fb.State == ConnectionState.Open)
             {
-                label5.Text = "Подключено";
-                label5.ForeColor = Color.Green;
+                Label_out.Text = "Подключено";
+                Label_out.ForeColor = Color.Green;
             }
             else
             {
-                label5.Text = "Что-то пошло не так..";
-                label5.ForeColor = Color.Red;
+                Label_out.Text = "Что-то пошло не так..";
+                Label_out.ForeColor = Color.Red;
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)//select
+        public void method_arr_of_users(ref List<List<string>> arr_out)//метод формирования массива пользователей
         {
+            
             if (fb.State == ConnectionState.Open)
             {
                 int i = 0, j = 0;
@@ -62,24 +73,23 @@ namespace SQL
                 FbCommand SelectSQL = new FbCommand("SELECT people.lname||' '||people.fname||' '||people.sname, people.peopleid,cards.cardnum FROM cards INNER JOIN people ON(people.peopleid = CARDS.peopleid)", fb); //задаем запрос на выборку
                 SelectSQL.Transaction = fbt;
                 FbDataReader reader = SelectSQL.ExecuteReader();
-                string select_result = "";
-
+                
                 List<string> row = new List<string>();
                 Int32 temp = reader.FieldCount;
-                arr_user = new List<List<string>>();
-                
+                arr_out = new List<List<string>>();
+
                 try
                 {
                     while (reader.Read()) //пока не прочли все данные выполняем... //select_result = select_result + reader.GetInt32(0 ).ToString() + ", " + reader.GetString(1) + "\n";
                     {
                         row = new List<string>();
-                        arr_user.Add(row);
-                        arr_user[i].Add("");
-                        arr_user[i].Add("");
-                        arr_user[i].Add("");
-                        arr_user [i][j]= reader.GetString(0).ToString();
-                        arr_user[i][j+1] = reader.GetString(1).ToString();
-                        arr_user[i][j + 2] = reader.GetString(2).ToString();
+                        arr_out.Add(row);
+                        arr_out[i].Add("");
+                        arr_out[i].Add("");
+                        arr_out[i].Add("");
+                        arr_out[i][j] = reader.GetString(0).ToString();
+                        arr_out[i][j + 1] = reader.GetString(1).ToString();
+                        arr_out[i][j + 2] = reader.GetString(2).ToString();
                         i++;
                     }
                 }
@@ -90,27 +100,187 @@ namespace SQL
                     fb.Close(); //закрываем соединение, т.к. оно нам больше не нужно
                 }
                 SelectSQL.Dispose();//в документации написано, что ОЧЕНЬ рекомендуется убивать объекты этого типа, если они больше не нужны
+            }
+        }
 
 
-                dataGridView1.RowCount = arr_user.Count;
-                dataGridView1.ColumnCount = 3;
-                for (int ii=0;ii<arr_user.Count;ii++)
+
+        public void method_arr_of_events(string date_to_request_in, ref List<List<string>> arr_out)
+        {
+            bool start_bool = true;
+            if (fb.State == ConnectionState.Open)
+            {
+                if (date_to_request_in == "0")
                 {
-                    for (int jj=0;jj<3;jj++)
+                    date_to_request_in = now.ToString("dd.MM.yyyy");
+                }
+
+                int i = 0, j = 0;
+
+                FbTransaction fbt = fb.BeginTransaction();
+                FbCommand SelectSQL = new FbCommand("SELECT DISTINCT events.eventsdate, events.cardnum,events.readerid FROM events WHERE events.eventsdate >= '" + date_to_request_in + " 00:00:00' AND events.eventsdate <= '" + date_to_request_in + " 23:59:59'", fb); //задаем запрос на выборку
+                SelectSQL.Transaction = fbt;
+                FbDataReader reader = SelectSQL.ExecuteReader();
+
+                List<string> row = new List<string>();
+                Int32 temp = reader.FieldCount;
+                arr_out = new List<List<string>>();
+
+                try
+                {
+                    while (reader.Read()) //пока не прочли все данные выполняем... //select_result = select_result + reader.GetInt32(0 ).ToString() + ", " + reader.GetString(1) + "\n";
                     {
-                        dataGridView1.Rows[ii].Cells[jj].Value = String.Format("{0}",arr_user[ii][jj]);
+                        row = new List<string>();
+                        arr_out.Add(row);
+                        arr_out[i].Add("");
+                        arr_out[i].Add("");
+                        arr_out[i].Add("");
+                        arr_out[i][j] = reader.GetString(0).ToString();//добавиои время
+                        arr_out[i][j + 1] = reader.GetString(1).ToString();// добавили id ключа
+                        arr_out[i][j + 2] = reader.GetString(2).ToString();//добавили id эвента
+                        for (int ii = 0; (ii < arr_out.Count - 1) && start_bool == false; ii++)// запуск цикла проверки на существующие запиви
+                        {
+                            if ((arr_out[ii][1] == arr_out[i][1])&& (arr_out[ii][2] == arr_out[i][2]))//проверка на уже существующее ID 
+                            {
+                                if (Convert.ToInt32(arr_out[i][2]) == 3)// проверка на сощуствующий эвент 3-вход 13-выход
+                                {
+                                    if (DateTime.Parse(arr_out[ii][0]) > DateTime.Parse(arr_out[i][0])) // проверка на время
+                                    {
+                                        arr_out[ii][0] = arr_out[i][0];
+                                    }
+                                }
+                                if (Convert.ToInt32(arr_out[i][2]) == 13)// провоека на сощуствующий эвент 3-вход 13-выход
+                                {
+                                    if (DateTime.Parse(arr_out[ii][0]) < DateTime.Parse(arr_out[i][0])) // проверка на время
+                                    {
+                                        arr_out[ii][0] = arr_out[i][0];
+                                    }
+                                }
+                                arr_out.Remove(row);
+                                i--;
+                            }
+                        }
+                        i++;
+                        start_bool = false;
+                    }
+                }
+                finally
+                {
+                    //всегда необходимо вызывать метод Close(), когда чтение данных завершено
+                    reader.Close();
+                    fb.Close(); //закрываем соединение, т.к. оно нам больше не нужно
+                }
+                SelectSQL.Dispose();//в документации написано, что ОЧЕНЬ рекомендуется убивать объекты этого типа, если они больше не нужны
+            }                                                  
+        }
+
+
+
+        public void method_arr_to_grid(List<List<string>> arr_in, ref DataGridView Grid_out)//метод вывывода массива в датагрид
+        {
+            if (arr_in.Count != 0)
+            {
+                Grid_out.RowCount = arr_in.Count;
+                Grid_out.ColumnCount = 3;
+                for (int ii = 0; ii < arr_in.Count; ii++)
+                {
+                    for (int jj = 0; jj < 3; jj++)
+                    {
+                        Grid_out.Rows[ii].Cells[jj].Value = String.Format("{0}", arr_in[ii][jj]);
                     }
 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Данных за данный период нет.");
+            }
+        }
+
+
+
+
+
+        //-------------
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            method_connect_to_fb(textBox1, textBox2, textBox3, ref label5);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arr_user = null;
+                if (fb.State != ConnectionState.Open)
+                {
+                    method_connect_to_fb(textBox1, textBox2, textBox3, ref label5);
+                }
+                method_arr_of_users(ref arr_user);
+                method_arr_to_grid(arr_user, ref dataGridView1);
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте подключение к БД", "Сообщение", MessageBoxButtons.OK);
+            }
+        }
+               
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arr_events = null;
+                if (fb.State != ConnectionState.Open)
+                {
+                    method_connect_to_fb(textBox1, textBox2, textBox3, ref label5);
+                }
+                method_arr_of_events(date_to_request, ref arr_events);
+                method_arr_to_grid(arr_events, ref dataGridView2);
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте подключение к БД", "Сообщение", MessageBoxButtons.OK);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            date_to_request = dateTimePicker1.Value.ToShortDateString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<string> row = new List<string>();
+            arr_of_work = new List<List<string>>();
+
+            for (int i=0;i<arr_user.Count;i++)
+            {
+                row = new List<string>();
+                arr_of_work.Add(row);
+                arr_of_work[i].Add("");
+                arr_of_work[i].Add("");
+                arr_of_work[i].Add("");
+                arr_of_work[i][0] = arr_user[i][0];
+                for (int ii = 0; ii < arr_events.Count; ii++)
+                {
+                     if ((arr_user[i][2] == arr_events[ii][1])&&(Convert.ToInt32(arr_events[ii][2])==3))
+                     {
+                        arr_of_work[i][1] = arr_events[ii][0];
+                     }
+                     if ((arr_user[i][2] == arr_events[ii][1]) && (Convert.ToInt32(arr_events[ii][2]) == 13))
+                     {                     
+                        arr_of_work[i][2] = arr_events[ii][0];
+                     }
                 }
 
 
 
-
             }
-        }
+            method_arr_to_grid(arr_of_work, ref dataGridView3);
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
+
 
         }
     }
