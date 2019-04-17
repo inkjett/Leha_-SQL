129661e02d7364653259604882ebc931d20a185a
inkjett <mail22sms@gmail.com>
2019-04-17 14:57:33 +0500

    запилил выпадающий список для состояния

--- SQL/Form4.Designer.cs
+++ SQL/Form4.Designer.cs
@@ -34,12 +34,13 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 191);
+            this.button1.Location = new System.Drawing.Point(164, 415);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(168, 23);
             this.button1.TabIndex = 0;
@@ -50,16 +51,16 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(28, 12);
+            this.listBox1.Location = new System.Drawing.Point(15, 31);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(261, 173);
+            this.listBox1.Size = new System.Drawing.Size(247, 277);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 234);
+            this.label1.Location = new System.Drawing.Point(12, 399);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(23, 13);
             this.label1.TabIndex = 2;
@@ -68,16 +69,16 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(385, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(265, 31);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(317, 259);
+            this.dataGridView1.Size = new System.Drawing.Size(397, 277);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(77, 325);
+            this.button2.Location = new System.Drawing.Point(2, 415);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(156, 23);
             this.button2.TabIndex = 4;
@@ -88,17 +89,29 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(594, 355);
+            this.label2.Location = new System.Drawing.Point(59, 399);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(512, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Внести изменения";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(674, 450);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
@@ -107,6 +120,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form4";
             this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -121,5 +135,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- SQL/Form4.cs
+++ SQL/Form4.cs
@@ -16,20 +16,28 @@ namespace SQL
     public partial class Form4 : Form
     {
         FbConnection fb;
-
+        
         public Form4()
         {
             InitializeComponent();
-        }        
-        private void button1_Click(object sender, EventArgs e)
+        }
+
+        private void Form4_Load(object sender, EventArgs e)//загрузка формы
         {
-            
             for (int i = 0; i < Program.f1.arr_user.Count; i++)
             {
                 int index = Convert.ToInt32(Program.f1.arr_user[i][1]);
-
                 listBox1.Items.Insert(i, Program.f1.arr_user[i][0]);
             }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
         }
 
 
@@ -46,34 +54,46 @@ namespace SQL
                 MessageBox.Show(e.Message, "Сообщение", MessageBoxButtons.OK);
             }
         }
-
-
-
-
+                     
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            dataGridView1.Columns.Clear();
+            DataGridViewComboBoxColumn boxcolum = new DataGridViewComboBoxColumn();
+            boxcolum.HeaderText = "Причина отсуствия";
+            boxcolum.DropDownWidth = 90;
+            boxcolum.Width = 90;
+            boxcolum.MaxDropDownItems = 4;
+            this.dataGridView1.Columns.Insert(0, boxcolum);
+            boxcolum.Items.AddRange("больничный", "отпуск", "командировка", "удаленная работа");            
+            //this.dataGridView1.Columns[0].DataPropertyName = "trans_type";
+
             dataGridView1.Rows.Clear();
             dataGridView1.ColumnCount = 4;
-            dataGridView1.Columns[0].Width = 90;
+            //dataGridView1.Columns[0].Width = 90;
+            //dataGridView1.Columns[0].HeaderText = "Причина отсуствия";
             dataGridView1.Columns[1].Width = 90;
+            dataGridView1.Columns[1].HeaderText = "Начальная дата";
             dataGridView1.Columns[2].Width = 90;
+            dataGridView1.Columns[2].HeaderText = "Конечная дата";
             dataGridView1.Columns[3].Width = 1;
-            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[3].Visible = false;      
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.LightGray;
             string User_ID = Program.f1.arr_user.Where(o => o.IndexOf(listBox1.SelectedItem.ToString()) != -1).FirstOrDefault()[1];// полчение ID выбранного пользователя(выбор+поиск по массиву пользователей)
             var temp = Program.f1.arr_of_deviation.Where(o => o[0] == User_ID).ToList();
-            var temp2 = temp;
 
             for (int i=0; i<temp.Count;i++)
             {
+                
                 dataGridView1.Rows.Add();
-                switch (Convert.ToInt16(temp[i][1]))
+                switch (Convert.ToInt16 (temp[i][1]))
                 {
                     case 0:
                         dataGridView1.Rows[i].Cells[0].Value = "больничный";
                         break;
                     case 1:
-                        dataGridView1.Rows[i].Cells[0].Value = "отпуск";
+                        dataGridView1.Rows[i].Cells[0].Value =  "отпуск";
                         break;
                     case 2:
                         dataGridView1.Rows[i].Cells[0].Value = "командировка";
@@ -82,9 +102,10 @@ namespace SQL
                         dataGridView1.Rows[i].Cells[0].Value = "удаленная работа";
                         break;
                 }
+                
                 dataGridView1.Rows[i].Cells[1].Value = temp[i][2];
                 dataGridView1.Rows[i].Cells[2].Value = temp[i][3];
-                dataGridView1.Rows[i].Cells[3].Value = temp[i][4];
+                dataGridView1.Rows[i].Cells[3].Value = temp[i][4];                
             }
             
                                                                               
@@ -102,71 +123,102 @@ namespace SQL
 
         private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-            string pattern = @"^[0-3]{1}[0-9]{1}.[0-1]{1}[0-9]{1}.[2]{1}[0-1]{1}[0-9]{2}$";//строка дял проверки ввода даты на формат XX.XX.XXXX
-            int reason_absence = -1;
-            bool can_run_query = false;
+            if (dataGridView1.RowCount-2 == e.RowIndex)
+            {
+                bool temp123 = true;
 
 
-            method_connect_to_fb(Program.f1.connecting_path);// проверка ввода 
-            if(Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "больничный")
-            {
-                reason_absence = 0;
-                can_run_query = true;
             }
 
-            if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "отпуск")
-            {
-                reason_absence = 1;
-                can_run_query = true;
-            }
-            if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "командировка")
-            {
-                reason_absence = 2;
-                can_run_query = true;
-            }
-            if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "удаленная работа")
-            {
-                reason_absence = 3;
-                can_run_query = true;
-            }
 
-            if ((Regex.IsMatch(Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value), pattern))&& can_run_query)
-            {
-                can_run_query = true;
-            }
-            else
-            {
-                can_run_query = false;
-            }
 
-            if ((Regex.IsMatch(Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value), pattern)) && can_run_query)
-            {
-                can_run_query = true;
-            }
-            else
-            {
-                can_run_query = false;
-            }
 
-            if (can_run_query)
+
+
+
+
+            if (checkBox1.Checked)
             {
-                FbCommand InsertSQL = new FbCommand("update deviation set deviation.devfrom='" + dataGridView1.Rows[e.RowIndex].Cells[1].Value + "', deviation.devto='" + dataGridView1.Rows[e.RowIndex].Cells[2].Value + "', deviation.devtype='" + reason_absence +  "'where deviation.deviationid='" + dataGridView1.Rows[e.RowIndex].Cells[3].Value + "'", fb); //задаем запрос на получение данных
-                if (fb.State == ConnectionState.Open)
+                string pattern = @"^[0-3]{1}[0-9]{1}.[0-1]{1}[0-9]{1}.[2]{1}[0-1]{1}[0-9]{2}$";//строка дял проверки ввода даты на формат XX.XX.XXXX
+                int reason_absence = -1;
+                bool can_run_query = false;
+                
+                method_connect_to_fb(Program.f1.connecting_path);// проверка ввода 
+                if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "больничный")
                 {
-                    FbTransaction fbt = fb.BeginTransaction(); //необходимо проинициализить транзакцию для объекта InsertSQL
-                    InsertSQL.Transaction = fbt;
-                    int result = InsertSQL.ExecuteNonQuery();
-                    MessageBox.Show("ВЫполнено" + result);
-                    fbt.Commit();
-                    fbt.Dispose();
-                    InsertSQL.Dispose();
+                    reason_absence = 0;
+                    can_run_query = true;
+                }
+
+                if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "отпуск")
+                {
+                    reason_absence = 1;
+                    can_run_query = true;
+                }
+                if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "командировка")
+                {
+                    reason_absence = 2;
+                    can_run_query = true;
+                }
+                if (Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value) == "удаленная работа")
+                {
+                    reason_absence = 3;
+                    can_run_query = true;
+                }
+
+                if ((Regex.IsMatch(Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value), pattern)) && can_run_query)
+                {
+                    can_run_query = true;
+                }
+                else
+                {
+                    can_run_query = false;
+                }
+
+                if ((Regex.IsMatch(Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value), pattern)) && can_run_query)
+                {
+                    can_run_query = true;
+                }
+                else
+                {
+                    can_run_query = false;
+                }
+
+                if (can_run_query)
+                {
+                    FbCommand InsertSQL = new FbCommand("update deviation set deviation.devfrom='" + dataGridView1.Rows[e.RowIndex].Cells[1].Value + "', deviation.devto='" + dataGridView1.Rows[e.RowIndex].Cells[2].Value + "', deviation.devtype='" + reason_absence + "'where deviation.deviationid='" + dataGridView1.Rows[e.RowIndex].Cells[3].Value + "'", fb); //задаем запрос на получение данных
+                    if (fb.State == ConnectionState.Open)
+                    {
+                        FbTransaction fbt = fb.BeginTransaction(); //необходимо проинициализить транзакцию для объекта InsertSQL
+                        InsertSQL.Transaction = fbt;
+                        int result = InsertSQL.ExecuteNonQuery();
+                        MessageBox.Show("ВЫполнено" + result);
+                        fbt.Commit();
+                        fbt.Dispose();
+                        InsertSQL.Dispose();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте введенные данные");
                 }
             }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked==true)
+            {
+                dataGridView1.ReadOnly = false;
+                dataGridView1.RowsDefaultCellStyle.BackColor = Color.White;
+            }
             else
             {
-                MessageBox.Show("Проверьте введенные данные");
+                dataGridView1.ReadOnly = true;
+                dataGridView1.RowsDefaultCellStyle.BackColor = Color.LightGray; 
             }
         }
-        
+
+
     }
 }
